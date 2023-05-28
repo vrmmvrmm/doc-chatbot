d202f520b52b00ad8426932f925854b61ca2ee98
Nick McCrory <n.mccrory@icloud.com>
2023-05-28 00:05:50 -0500

    uses original mayooear prompt

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -27,14 +27,12 @@ import { ConversationalRetrievalQAChain } from 'langchain/chains';
 // --------------------------------------------------
 
 // v3
-const CONDENSE_PROMPT = `Condense the chat history and the follow-up question into a standalone question.
+const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
-
-Follow-up input: {question}
-
-Rephrased standalone question:`;
+Follow Up Input: {question}
+Standalone question:`;
 
 // --------------------------------------------------
 
@@ -60,13 +58,14 @@ Rephrased standalone question:`;
 // --------------------------------------------------
 
 // v3
-const QA_PROMPT = `As an AI, answer this document-based question. If uncertain or off-topic, admit it.
+const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
+If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
 
-Context: {context}
+{context}
 
 Question: {question}
-
-Answer in markdown. Be concise, follow the instructions in the question to the letter. If unsure, say "Uncertain based on the documents". Answer:`;
+Helpful answer in markdown:`;
 
 // Creates a ConversationalRetrievalQAChain object that uses an OpenAI model and a PineconeStore vectorstore
 export const makeChain = (
